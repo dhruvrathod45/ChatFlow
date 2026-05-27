95fc2f3a73deea4179bc1f44df5fed5179fdc89b
dhruv45396 <dhruv47rathod@gmail.com>
2026-05-27 13:44:19 +0530

    Final production fix

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,18 @@ const { Server } = require("socket.io");
 dotenv.config();
 
 const authRoutes = require("./routes/authRoutes");
-
 const Message = require("./models/messageModel");
 
 const app = express();
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"]
+}));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 const server = http.createServer(app);
 
@@ -22,37 +30,35 @@ const io = new Server(server, {
   }
 });
 
-app.use(cors());
-
-app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
-app.use("/api/auth", authRoutes);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB Connected");
+})
+.catch((err) => {
+  console.log(err);
+});
 
 let onlineUsers = 0;
 
 io.on("connection", (socket) => {
-
-  onlineUsers++;
-
-  io.emit("online-users", onlineUsers);
-
   console.log("User Connected:", socket.id);
 
-  socket.on("new-user", (username) => {
+  onlineUsers++;
+  io.emit("online-users", onlineUsers);
 
-    socket.broadcast.emit("user-joined", username);
-
+  socket.on("join-chat", (username) => {
+    io.emit("receive-message", {
+      username: "System",
+      message: `${username} joined the chat`,
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit"
+      })
+    });
   });
 
   socket.on("send-message", async (data) => {
-
     try {
-
       const newMessage = new Message({
         username: data.username,
         message: data.message,
@@ -61,32 +67,24 @@ io.on("connection", (socket) => {
 
       await newMessage.save();
 
-      socket.broadcast.emit("receive-message", data);
+      io.emit("receive-message", data);
 
     } catch (error) {
-
-      console.log(error.message);
-
+      console.log(error);
     }
-
   });
 
   socket.on("disconnect", () => {
+    console.log("User Disconnected");
 
     onlineUsers--;
 
     io.emit("online-users", onlineUsers);
-
-    console.log("User Disconnected");
-
   });
-
 });
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-
   console.log(`Server running on port ${PORT}`);
-
 });
